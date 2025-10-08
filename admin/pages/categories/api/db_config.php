f4f0c145b9b1c7bc246674b9759c29f340fb7e08
Haseeb-7884 <usmananees040@gmail.com>
2025-10-08 22:17:18 +0500

    update-db-credentials-admin

--- admin/pages/categories/api/db_config.php
+++ admin/pages/categories/api/db_config.php
@@ -1,6 +1,6 @@
 <?php
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'mcqs_database');
-define('DB_USER', 'root'); // Replace with your database username
-define('DB_PASS', ''); // Replace with your database password
+define('DB_HOST', 'sql210.infinityfree.com');
+define('DB_NAME', 'if0_40111840_mcqs_database');
+define('DB_USER', 'if0_40111840'); // Replace with your database username
+define('DB_PASS', 'Haseeb7884'); // Replace with your database password
 ?>
